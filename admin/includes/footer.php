5d1bcedb815c6e6fbfeb63727e57dbcf276e407b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-04-25 16:17:25 +0500

    create admin Photo Edit method

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,12 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- WYSIWYG -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.7.1/tinymce.min.js" integrity="sha512-RnlQJaTEHoOCt5dUTV0Oi0vOBMI9PjCU7m+VHoJ4xmhuUNcwnB5Iox1es+skLril1C3gHTLbeRepHs1RpSCLoQ==" crossorigin="anonymous"></script>
+
+    <!-- Scripts JS -->
+    <script src="js/scripts.js"></script>
+
 </body>
 
 </html>
--- admin/photos.php
+++ admin/photos.php
@@ -37,8 +37,8 @@
                                     <td>
                                         <img src="<?php echo $photo->picture_path(); ?>" alt="" style="max-width: 100px;">
                                         <div class="pictures_link">
-                                            <a href="delete_photo.php/?id=<?php echo $photo->id; ?>">Delete</a>
-                                            <a href="#">Edit</a>
+                                            <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
+                                            <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
                                             <a href="#">View</a>
                                         </div>
                                     </td>
